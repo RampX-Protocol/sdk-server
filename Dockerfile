c818485b1e36f93b25aef04ad2dca287db96c4cd
mrlotfi <mrlotfi@MacBook-Pro.local>
2024-09-05 16:23:00 +0330

    Implement server for tx gneeration

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# create a dockerfile that builds nodejs image and compiles main.ts and runs the compiled main.js
+FROM node:20-bullseye-slim As build
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npx tsc
+
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
---
+++ main.ts
@@ -0,0 +1,175 @@
+import {
+	ChainName,
+	createSwapFromSolanaInstructions,
+	fetchQuote,
+	getSwapFromEvmTxPayload,
+} from "@mayanfinance/swap-sdk";
+import { Connection } from "@solana/web3.js";
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+const chainNames = [
+  "solana",
+  "ethereum",
+  "bsc",
+  "polygon",
+  "avalanche",
+  "arbitrum",
+  "optimism",
+  "base",
+];
+
+const chainNameToId: any = {
+  solana: 0,
+  ethereum: 1,
+  bsc: 56,
+  polygon: 137,
+  avalanche: 43114,
+  arbitrum: 42161,
+  optimism: 10,
+  base: 8453,
+};
+
+app.get("/solana", async (req: Request, res: Response) => {
+  try {
+    const amountIn = Number(req.query.amountIn);
+    const fromToken = req.query.fromToken!.toString();
+    const toToken = req.query.toToken!.toString();
+    const fromChain = req.query.fromChain!.toString();
+    const toChain = req.query.toChain!.toString();
+    const slippageBps = Number(req.query.slippageBps);
+    const gasDrop = Number(req.query.gasDrop);
+    const referrerBps = Number(req.query.referrerBps);
+    const evmReferrer = req.query.evmReferrer!.toString();
+    const solanaReferrer = req.query.solanaReferrer!.toString();
+    if (!chainNames.includes(fromChain) || !chainNames.includes(toChain)) {
+      res.status(406).send("Invalid chain name");
+      return;
+    }
+    const quotes = await fetchQuote(
+      {
+        amount: Number(amountIn),
+        fromToken: fromToken!.toString(),
+        toToken: toToken!.toString(),
+        fromChain: fromChain as ChainName,
+        toChain: toChain as ChainName,
+        slippageBps: slippageBps,
+        gasDrop: gasDrop,
+        referrerBps: referrerBps,
+        referrer: fromChain === "solana" ? solanaReferrer : evmReferrer,
+      },
+      {
+        gasless: false,
+        mctp: false,
+        onlyDirect: false,
+        swift: true,
+      }
+    );
+
+    const swapperWallet = req.query.swapperAddress!.toString();
+    const destAddress = req.query.destAddress!.toString();
+    const swap = await createSwapFromSolanaInstructions(
+      quotes[0],
+      swapperWallet,
+      destAddress,
+      {
+        evm: evmReferrer,
+        solana: solanaReferrer,
+      },
+      new Connection("https://api.mainnet-beta.solana.com")
+    );
+
+    let instructions: {
+      programId: string;
+      data: string;
+      accounts: { isSigner: boolean; isWritable: boolean; pubkey: string }[];
+    }[] = [];
+    for (let ix of swap.instructions) {
+      instructions.push({
+        programId: ix.programId.toString(),
+        data: ix.data.toString("base64"),
+        accounts: ix.keys.map((k) => ({
+          isSigner: k.isSigner,
+          isWritable: k.isWritable,
+          pubkey: k.pubkey.toString(),
+        })),
+      });
+    }
+
+    res.json({
+      addressLookupTableAddresses: swap.lookupTables.map((lt) =>
+        lt.key.toString()
+      ),
+      instructions: instructions,
+    });
+  } catch (err: any) {
+    console.error(err, err.stack);
+    res.status(500).send(err);
+  }
+});
+
+app.get("/evm", async (req: Request, res: Response) => {
+  try {
+    const amountIn = Number(req.query.amountIn);
+    const fromToken = req.query.fromToken!.toString();
+    const toToken = req.query.toToken!.toString();
+    const fromChain = req.query.fromChain!.toString();
+    const toChain = req.query.toChain!.toString();
+    const slippageBps = Number(req.query.slippageBps);
+    const gasDrop = Number(req.query.gasDrop);
+    const referrerBps = Number(req.query.referrerBps);
+    const evmReferrer = req.query.evmReferrer!.toString();
+    const solanaReferrer = req.query.solanaReferrer!.toString();
+    if (!chainNames.includes(fromChain) || !chainNames.includes(toChain)) {
+      res.status(406).send("Invalid chain name");
+      return;
+    }
+    const quotes = await fetchQuote(
+      {
+        amount: Number(amountIn),
+        fromToken: fromToken!.toString(),
+        toToken: toToken!.toString(),
+        fromChain: fromChain as ChainName,
+        toChain: toChain as ChainName,
+        slippageBps: slippageBps,
+        gasDrop: gasDrop,
+        referrerBps: referrerBps,
+        referrer: fromChain === "solana" ? solanaReferrer : evmReferrer,
+      },
+      {
+        gasless: false,
+        mctp: false,
+        onlyDirect: false,
+        swift: true,
+      }
+    );
+
+    const swapperWallet = req.query.swapperAddress!.toString();
+    const signerAddress = swapperWallet;
+    const destAddress = req.query.destAddress!.toString();
+    const swap = getSwapFromEvmTxPayload(
+      quotes[0],
+      swapperWallet,
+      destAddress,
+      {
+        evm: evmReferrer,
+        solana: solanaReferrer,
+      },
+      signerAddress,
+	  chainNameToId[fromChain],
+	  null,
+	  null,
+    );
+
+
+    res.json(swap);
+  } catch (err: any) {
+    console.error(err, err.stack);
+    res.status(500).send(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
